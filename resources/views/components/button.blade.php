df6bded32e294853fbccb68f41c8a3022b5ebb77
Dovias <70913423+Dovias@users.noreply.github.com>
2024-12-03 13:22:34 +0200

    Adjust generic button and modal styles

--- resources/views/components/button.blade.php
+++ resources/views/components/button.blade.php
@@ -6,7 +6,7 @@
 
     $_significance = [
         '100' => 'rounded-md transition hover:bg-gray-100 active:bg-gray-200',
-        '200' => 'bg-blue-700 text-white rounded-md active:bg-blue-800 transition'
+        '200' => 'bg-blue-700 text-white rounded-md hover:bg-blue-800 active:bg-blue-900 transition'
     ];
 @endphp
 
