6c1b1617cc5ead6edf53727d5f257edc8ed68c01
Dovias <70913423+Dovias@users.noreply.github.com>
2024-12-05 04:19:42 +0200

    Sanitize locale inside app template layout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="{!! App::currentLocale() !!}">
+<html lang="{{ App::currentLocale() }}">
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
