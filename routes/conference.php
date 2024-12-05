a43c87b2dc97c33041eafebeed73b5f267910323
Dovias <70913423+Dovias@users.noreply.github.com>
2024-12-05 13:44:55 +0200

    Fix broken add conference section title

--- routes/conference.php
+++ routes/conference.php
@@ -6,8 +6,8 @@ use Illuminate\Support\Facades\Route;
 Route::view('/conferences', 'conferences')->name('conferences');
 
 Route::middleware('auth')->group(function () {
-    Route::view('/conference/add', 'conference.add');
-    Route::post('/conference/add', [ConferenceController::class, 'add'])->name('conference.add');
+    Route::view('/conference/add', 'conference.add')->name('conference.add');
+    Route::post('/conference/add', [ConferenceController::class, 'add']);
     Route::delete('/conference/{conference}', [ConferenceController::class, 'delete']) -> name('conference.delete');
 });
 
